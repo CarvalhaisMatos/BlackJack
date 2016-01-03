81d94300f5b4658a3c05cc0b18c4a90e2ebb29ea
Diogo Gomes <dgomes@av.it.pt>
2016-01-03 18:15:44 +0000

    works

--- casino.py
+++ casino.py
@@ -1,15 +1,26 @@
 from game import Game
 from player import Player
-from randomplayer import RandomPlayer
+from proxyplayer import ProxyPlayer
+import sys 
 
 if __name__ == '__main__':
+    
+    players = []
 
-    players = [Player("Human",100)]
+    for nmec in sys.argv[1:]:
+        players += [ProxyPlayer(int(nmec), nmec, 100)]
 
-    for i in range(100):
-        print players
-        g = Game(players, min_bet=1, max_bet=5) 
-        #g = Game(players, debug=True)
-        g.run()
+    for p in players:
+        p.new()
+
+    if len(players):
+        for i in range(2):
+            print players
+            g = Game(players, min_bet=1, max_bet=5) 
+            #g = Game(players, debug=True)
+            g.run()
 
     print "OVERALL: ", players
+
+    for p in players:
+        p.bye()
