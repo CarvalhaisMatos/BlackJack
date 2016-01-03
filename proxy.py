1366a69cc9ebc701760978b1fc91f378770dd12d
Diogo Gomes <dgomes@av.it.pt>
2016-01-03 18:11:45 +0000

    Moved to HTTPServer

--- proxy.py
+++ proxy.py
@@ -4,45 +4,90 @@ __email__ = 'dgomes@ua.pt'
 __license__ = "GPL"
 __version__ = "0.1"
 import json 
-import zmq
 from player import Player
 from student import StudentPlayer
-from json_utils import object_to_dict
-from json_utils import dict_to_object
 from game import Game
+import sys
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
-context = zmq.Context()
+def dict_to_object(d):
+    if '__class__' in d:
+        class_name = d.pop('__class__')
+        module_name = d.pop('__module__')
+        module = __import__(module_name)
+#        print 'MODULE:', module
+        if class_name in ['Rules', 'PlayerState']:
+            class_base = getattr(module, "Game")
+            class_ = getattr(class_base, class_name)
+        elif class_name == 'ProxyPlayer':
+            module = __import__('player')
+            class_ = getattr(module, 'Player')
+        else:
+            class_ = getattr(module, class_name)
 
-#  Socket to talk to server
-print("Connecting to Casino…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
-
-p = Player("Human", 100)
-
-print("Sending request …")
-socket.send("HELLO" + str(p))
-
-#  Get the reply.
-message = socket.recv()
-print("Received reply [{}]".format(message))
-socket.send("OK" + str(p))
-while True:
-    msg = json.loads(socket.recv(), object_hook=dict_to_object)
-
-    if msg['cmd'] == 'SHOW':
-        p.show(msg['players'])
-        socket.send("OK")
-    elif msg['cmd'] == 'WANT_TO_PLAY':
-        socket.send(json.dumps(p.want_to_play(msg['rules'])))
-    elif msg['cmd'] == 'PAYBACK':
-        p.payback(msg['prize'])
-        socket.send("OK")
-    elif msg['cmd'] == 'PLAY':
-        socket.send(json.dumps(p.play(msg['dealer'], msg['players'])))
-    elif msg['cmd'] == 'BET':
-        socket.send(json.dumps(p.bet(msg['dealer'], msg['players'])))
+#        print 'CLASS:', class_
+        args = dict( (key.encode('ascii'), value) for key, value in d.items())
+#        print 'INSTANCE ARGS:', args
+        inst = class_(**args)
     else:
-        print("ERROR UNKNOWN MESSAGE:")
-        print msg
-        socket.send("ERROR")
+        inst = d
+    return inst
+
+KEEP_RUNNING = True
+p = None
+
+
+class PlayerRequestHandler (BaseHTTPRequestHandler):
+    def do_POST(self):
+        global p
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+        msg = json.loads(self.data_string, object_hook=dict_to_object)
+        ret = "FAIL"
+        code = 200
+
+        if msg['cmd'] == 'SHOW':
+            p.show(msg['players'])
+            ret = "OK"
+        elif msg['cmd'] == 'WANT_TO_PLAY':
+            ret = p.want_to_play(msg['rules'])
+        elif msg['cmd'] == 'PAYBACK':
+            p.payback(msg['prize'])
+            ret = p.pocket
+        elif msg['cmd'] == 'PLAY':
+            ret = p.play(msg['dealer'], msg['players'])
+        elif msg['cmd'] == 'BET':
+            ret = p.bet(msg['dealer'], msg['players'])
+        elif msg['cmd'] == 'BYE':
+            ret = "BYE"
+        elif msg['cmd'] == 'NEW':
+            p = Player(msg['name'], msg['money'])
+            ret = "READY"
+        else:
+            print("ERROR UNKNOWN MESSAGE:")
+            print(msg)
+            ret = "ERROR"
+            code = 500
+
+        self.send_response(code)
+        self.end_headers()
+        self.wfile.write(json.dumps(ret))
+       
+        if ret == "BYE":
+            print "BYE"
+            global KEEP_RUNNING
+            KEEP_RUNNING = False
+try:
+    nmec = sys.argv[1]
+    PORT_NUMBER = int(nmec)
+    
+    server = HTTPServer(('', PORT_NUMBER), PlayerRequestHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+    
+    #Wait forever for incoming htto requests
+    while KEEP_RUNNING:
+        server.handle_request()
+
+except KeyboardInterrupt:
+    print '^C received, shutting down the web server'
+    server.socket.close()
--- proxyplayer.py
+++ proxyplayer.py
@@ -5,43 +5,59 @@ __license__ = "GPL"
 __version__ = "0.1"
 import json
 from player import Player
-from json_utils import object_to_dict
-from json_utils import dict_to_object
-import zmq
+import requests
+
+def object_to_dict(obj):
+    d = { '__class__':obj.__class__.__name__, 
+          '__module__':obj.__module__,
+        }
+    d.update(obj.__dict__)
+    if d['__class__'] == 'ProxyPlayer':
+        d.pop('identity')
+    return d
 
 class ProxyPlayer(Player):
-    def __init__(self, socket, name="Player", money=0):
-        self.name = name
-        self.pocket = money #dont mess with pocket!
-        self.table = 0
-        self.socket = socket
+    def _issue_cmd(self, cmd):
+        data = json.dumps(cmd, default=object_to_dict)
+        headers = {'content-type': 'application/json'}
+        r = requests.post("http://localhost:{}/post".format(self.identity), data = data, headers = headers)
+        print "[{}] -> {}".format(self.identity, r.text)
+        message = json.loads(r.text)
+        return message
+
+    def __init__(self, identity, name="Player", money=0):
+        super(ProxyPlayer, self).__init__(name=name, money=money)
+        self.identity = identity
+
+    def new(self):
+        self._issue_cmd({'cmd':"NEW", 'name':self.name, 'money':self.pocket})
 
     def show(self, players):
         print "show()"
-        self.socket.send(json.dumps({'cmd':"SHOW", 'players':players}, default=object_to_dict))
-        message = self.socket.recv()
+        self._issue_cmd({'cmd':"SHOW", 'players':players})
 
     def want_to_play(self, rules):     
         print "want_to_play()"
-        self.socket.send(json.dumps({'cmd':"WANT_TO_PLAY", 'rules':rules}, default=object_to_dict))
-        message = self.socket.recv()
-        return json.loads(message)
+        message = self._issue_cmd({'cmd':"WANT_TO_PLAY", 'rules':rules})
+        print ">{}<".format(message)
+        return message
 
     def payback(self, prize):
         print "payback()"
-        self.socket.send(json.dumps({'cmd':"PAYBACK", 'prize':prize}))
-        message = self.socket.recv()
+        message = self._issue_cmd({'cmd':"PAYBACK", 'prize':prize})
+        self.pocket = message
 
     def play(self, dealer, players):
         print "play()"
         self.debug_state(dealer, players)
-        self.socket.send(json.dumps({'cmd':"PLAY", 'dealer':dealer, 'players':players}, default=object_to_dict))
-        message = self.socket.recv()
-        return json.loads(message)
+        message = self._issue_cmd({'cmd':"PLAY", 'dealer':dealer, 'players':players})
+        return message
 
     def bet(self, dealer, players):
         print "bet()"
         self.debug_state(dealer, players)
-        self.socket.send(json.dumps({'cmd':"BET", 'dealer':dealer, 'players':players}, default=object_to_dict))
-        message = self.socket.recv()
-        return json.loads(message)
+        message = self._issue_cmd({'cmd':"BET", 'dealer':dealer, 'players':players})
+        return message
+    def bye(self):
+        print "bye()"
+        self._issue_cmd({'cmd':"BYE"})
